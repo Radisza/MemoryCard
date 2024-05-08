04f9b6acc5fe91fa846eeef64999096244ba6159
Ela <radisza5@gmail.com>
2024-05-08 12:55:25 +0200

    Add userPanel with all the buttons to set the game

--- src/Game.jsx
+++ src/Game.jsx
@@ -3,11 +3,8 @@ import { fetchNewPictures } from './Pictures';
 import { Board } from './Board';
 import { gameStatusReducer, initialGameStatus } from './GameStatus';
 import './Game.css';
-import {
-  ImageTopics,
-  NO_IMAGE_TOPIC,
-  DEFAULT_IMAGE_TOPIC,
-} from './ImageTopics';
+import { NO_IMAGE_TOPIC, DEFAULT_IMAGE_TOPIC } from './ImageTopics';
+import { UserPanel } from './UserPanel';
 
 const MIN_IMAGES_NUM = 10;
 const IMAGES_LEVEL_RATIO = 5;
@@ -62,21 +59,12 @@ export function Game() {
   }
   return (
     <>
-      <button
-        className="fetchBtn"
-        id="newGameBtn"
-        onClick={() => handleNewLevel(0)}
-      >
-        New game
-      </button>
-      <button className="fetchBtn" onClick={handleNewImages}>
-        New images
-      </button>
-      <button className="fetchBtn" onClick={handleResetRound}>
-        Reset round
-      </button>
-
-      <ImageTopics setImagesTopic={setImagesTopic} />
+      <UserPanel
+        setNewLevel={handleNewLevel}
+        fetchNewImages={handleNewImages}
+        resetRound={handleResetRound}
+        setImagesTopic={setImagesTopic}
+      />
       <div> Images left: {gameStatus.picturesLeft.size} </div>
       <div> Game state: {gameStatus.state.stateToString()} </div>
       <div> Level: {gameStatus.level} </div>
---
+++ src/UserPanel.jsx
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+import { ImageTopics } from './ImageTopics';
+
+export function UserPanel({
+  setNewLevel,
+  fetchNewImages,
+  resetRound,
+  setImagesTopic,
+}) {
+  return (
+    <div className="UserPanel">
+      <button
+        className="fetchBtn"
+        id="newGameBtn"
+        onClick={() => setNewLevel(0)}
+      >
+        New game
+      </button>
+      <button className="fetchBtn" onClick={fetchNewImages}>
+        New images
+      </button>
+      <button className="fetchBtn" onClick={resetRound}>
+        Reset round
+      </button>
+
+      <ImageTopics setImagesTopic={setImagesTopic} />
+    </div>
+  );
+}
+
+UserPanel.propTypes = {
+  setNewLevel: PropTypes.func.isRequired,
+  fetchNewImages: PropTypes.func.isRequired,
+  resetRound: PropTypes.func.isRequired,
+  setImagesTopic: PropTypes.func.isRequired,
+};
